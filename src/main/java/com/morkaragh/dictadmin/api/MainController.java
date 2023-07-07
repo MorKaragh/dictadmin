da1c8595c55b10c4c8dd3faa1f54e837d41c37f5
MorKaragh <hentai.exe@gmail.com>
2023-07-07 22:32:04 +0300

    Api

--- src/main/java/com/morkaragh/dictadmin/api/MainController.java
+++ src/main/java/com/morkaragh/dictadmin/api/MainController.java
@@ -1,7 +1,18 @@
 package com.morkaragh.dictadmin.api;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Profile("api")
+@RestController
+@RequestMapping("api")
 public class MainController {
+
+    @GetMapping("test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("hello, world");
+    }
+
+
 }
--- src/main/java/com/morkaragh/dictadmin/rules/KeyValueRulesRepository.java
+++ src/main/java/com/morkaragh/dictadmin/rules/KeyValueRulesRepository.java
@@ -1,8 +1,6 @@
 package com.morkaragh.dictadmin.rules;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -10,12 +8,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@RepositoryRestResource
+//@RepositoryRestResource
 public interface KeyValueRulesRepository extends CrudRepository<KeyValueRule, Long> {
 
     Optional<KeyValueRule> findByCreatedBy_LoginIgnoreCase(String login);
 
-    @RestResource(path = "byKey", rel = "findByKey")
+//    @RestResource(path = "byKey", rel = "findByKey")
     List<KeyValueRule> findByEndDateNullAndKey(@NonNull String key);
 
 
